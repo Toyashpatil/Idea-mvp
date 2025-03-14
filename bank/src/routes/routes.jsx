197f1f62c26cede94c5c1d7ec0dc29638534c248
Toyash <toyashpatil17@gmail.com>
2025-03-14 13:44:17 +0530

    feat:screens and routes

---
+++ bank/src/routes/routes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "../screens/Home";
+
+
+const routes =[
+    {
+        path : "/",
+        element: <Home/>,
+    }, 
+   
+
+];
+export default routes;
